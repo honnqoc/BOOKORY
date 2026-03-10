6d68b0768da4e7261e03b1863f747fa01ebbf957
honnqoc <dangthihongngoc33@gmail.com>
2026-03-10 21:59:51 +0700

    update html, css, js

--- sanpham/logic.js
+++ sanpham/logic.js
@@ -1,28 +1,29 @@
 //BANNER
-let bannerSlider = document.querySelector(".home-banner__list");
-let bannerItems = document.querySelectorAll(".home-banner");
-let dot = document.querySelectorAll(".dots li");
-let active = 0;
+let bannerSlider = document.querySelector(".banner__list");
+let bannerItems = document.querySelectorAll(".banner");
+let dot = document.querySelectorAll(".banner__dots li");
+let active = 0; //banner đang hiển thị
 let soBanner = bannerItems.length;
 
-// hiển thị banner
-function showBanner(index){
-    active = index;
-    // trượt banner
-    bannerSlider.style.transform = `translateX(-${active * 100}%)`;
-    // đổi dots
-    dot.forEach(d => d.classList.remove("banner__dot-action"));
-    dot[active].classList.add("banner__dot-action");
-}
-
-// click dots
+// click vào dot nào sẽ cho ra một index để chạy banner tương ứng
 dot.forEach((d, index) => {
     d.addEventListener("click", () => {
         showBanner(index);
     });
 });
 
-// auto chạy banner
+// hiển thị banner
+function showBanner(index){
+    active = index;
+    // trượt banner container sang trái một khoảng bằng index*100%
+    bannerSlider.style.transform = `translateX(-${active * 100}%)`;
+    // đổi dots
+        //xóa active của tất cả các dots
+    dot.forEach(d => d.classList.remove("banner__dot-action"));
+        //thêm active cho dot đang hoạt động
+    dot[active].classList.add("banner__dot-action");
+}
+// auto chạy banner sau 6s
 setInterval(() => {
     active++;
     if(active >= soBanner){
@@ -32,102 +33,104 @@ setInterval(() => {
 }, 6000);
 
 // MỤC SẢN PHẨM HOT
-let hotLeft = document.querySelector(".home-hot__button-left");
-let hotRight = document.querySelector(".home-hot__button-right");
+let hotLeft = document.querySelector(".hot__button-left");
+let hotRight = document.querySelector(".hot__button-right");
 let hotSlider = document.querySelector(".hot-product__container");
 let hotSliderItems = document.querySelectorAll(".hot__product-card");
-
 let index = 0;
 const gap = 20;
 const hotSliderItemLength = hotSliderItems.length; //số lượng sản phẩm của mục hot
 
-// Lắng nghe sự kiện click
+// Click trái slider lùi 1
 hotLeft.addEventListener("click", function() {
-    handleChangeClick(-1);
+    hotSliderFunction(-1);
 });
-
+// Click phải slider tiến 1
 hotRight.addEventListener("click", function() {
-    handleChangeClick(1);
+    hotSliderFunction(1);
 });
 
-function handleChangeClick(param) {
+function hotSliderFunction(x) {
+    //chiều rộng của một thẻ
     const cardWidth = hotSliderItems[0].offsetWidth;
+    //khoảng cách di chuyển = chiều rộng của một thẻ + khoảng cách của các thẻ
     const moveDistance = cardWidth + gap;
-
+    //tính số card có thể hiển thị bằng chiều rộng của container chia cho thẻ card
     const visibleCards = Math.floor(
-        document.querySelector(".home-hot-product").offsetWidth / cardWidth
+        document.querySelector(".hot-product").offsetWidth / cardWidth
     );
-
+    //số index (lần di chuyển tối đa)= số card - số card hiển thị
     const maxIndex = hotSliderItemLength - visibleCards;
-
-    if (param === 1) {
+    //nếu x=1 trượt sang phải
+    if (x === 1) {
         index++;
+        //nếu hết sản phẩm quay về ban đầu
         if (index > maxIndex) index = 0;
     }
-
-    if (param === -1) {
+    //nếu x=-1 trượt sang trái
+    if (x === -1) {
         index--;
+        //nếu là sản phẩm đầu quay về sản phẩm cuối
         if (index < 0) index = maxIndex;
     }
-
+    //tính khoảng cách slider cần dịch chuyển = index * khoảng cách di chuyển mỗi lần    
     let positionX = -index * moveDistance;
-
-    hotSlider.style.transition = "transform 0.5s ease-in-out";
+    //di chuyển bằng cách thay đổi thuộc tính css
+    hotSlider.style.transition = "transform 0.5s";
     hotSlider.style.transform = `translateX(${positionX}px)`;
     
 }
 
 
-let partnerLeft = document.querySelector(".home-partner__button-left");
-let partnerRight = document.querySelector(".home-partner__button-right");
+let partnerLeft = document.querySelector(".partner__button-left");
+let partnerRight = document.querySelector(".partner__button-right");
 let partnerSlider = document.querySelector(".partner__img");
 let partnerSliderItems = document.querySelectorAll(".partner__img img");
 
-let indexp = 0;
-const gapp = 20;
+let indexPartner = 0;
+const gapPartner = 20;
 const partnerSliderItemLength = partnerSliderItems.length;
-
-// Lắng nghe sự kiện click
-let partnerContainer = document.querySelector(".home-partner");
-
+let partnerContainer = document.querySelector(".partner");
+// Click trái slider lùi 1
 partnerLeft.addEventListener("click", function() {
-    handleParnterSlide(-1);
+    partnerSliderFunction(-1);
 });
-
+//Click phải slider tiến 1
 partnerRight.addEventListener("click", function() {
-    handleParnterSlide(1);
+    partnerSliderFunction(1);
 });
 
-function handleParnterSlide(param) {
-
-    const cardWidth = partnerSliderItems[0].offsetWidth;
-    const moveDistance = cardWidth + gapp;
-
+function partnerSliderFunction(x) {
+    //chiều rộng của một thẻ
+    const partnerImgWidth = partnerSliderItems[0].offsetWidth;
+    //khoảng cách di chuyển = chiều rộng thẻ + khoảng cách giữa các thẻ
+    const moveDistance = partnerImgWidth + gapPartner;
+    //tính số card có thể hiển thị bằng chiều rộng của container chia cho thẻ card
     const visibleCards = Math.floor(
-        partnerContainer.offsetWidth / (cardWidth + gapp)
+        partnerContainer.offsetWidth / (partnerImgWidth + gapPartner)
     );
-
+    //số index (lần di chuyển tối đa)= số card - số card hiển thị
     const maxIndex = partnerSliderItemLength - visibleCards;
-
-    if (param === 1) {
-        indexp++;
-        if (indexp > maxIndex) indexp = 0;
+    //nếu x=1 trượt sang phải
+    if (x == 1) {
+        indexPartner++;
+        if (indexPartner > maxIndex) indexPartner = 0;
     }
-
-    if (param === -1) {
-        indexp--;
-        if (indexp < 0) indexp = maxIndex;
+    //nếu x=-1 trượt sang trái
+    if (x == -1) {
+        indexPartner--;
+        if (indexPartner < 0) indexPartner = maxIndex;
     }
-
-    let positionX = -indexp * moveDistance;
-
-    partnerSlider.style.transition = "transform 0.5s ease-in-out";
+    //tính khoảng cách slider cần dịch chuyển = index * khoảng cách di chuyển mỗi lần    
+    let positionX = -indexPartner * moveDistance;
+    //di chuyển bằng các thay đổi thuộc tính css
+    partnerSlider.style.transition = "transform 0.5s";
     partnerSlider.style.transform = `translateX(${positionX}px)`;
 }
 //NÚT XEM THÊM SẢN PHẨM==============================================================
 
 let products = document.querySelectorAll(".khampha__product-card");
-let moreButton = document.querySelector(".home-more__button");
+let moreButton = document.querySelector(".more__button");
 let hienThi = 10; //chỉ hiển thị 10 sản phẩm đầu tiên
 for(let i=hienThi; i < products.length; i++){
     //ẩn các sản phẩm sau sản phẩm thứ 10
